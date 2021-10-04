1f267a2d56a4a392d47f3453227d9dcdc8970c78
Alexander Bernard <abernard@gmx.de>
2021-10-04 22:10:47 +0200

    Fixed pipeline

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace weatherapi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "xFreezing", "xBracing", "xChilly", "xCool", "xMild", "xWarm", "xBalmy", "xHot", "xSweltering", "xScorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
--- main.tf
+++ main.tf
@@ -20,6 +20,11 @@ provider "azurerm" {
     features {}
 }
 
+variable "imagebuild" {
+  type = string
+  description = "Latest Image Build"
+}
+
 resource "azurerm_resource_group" "tf_test" {
     name = "tfmainrg"
     location = "West Europe"            
@@ -36,7 +41,7 @@ resource "azurerm_container_group" "tfcg_test" {
 
     container {
         name = "weatherapi"
-        image = "abernard/weatherapi"
+        image = "abernard/weatherapi:${var.imagebuild}"
           cpu = "1"
           memory = "1"
 
